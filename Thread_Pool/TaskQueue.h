6d71f5add3eafe07fbabbccb79a4d96d80210e72
c1336658570 <1336658570@qq.com>
2022-04-26 22:03:20 +0800

    1

---
+++ Thread_Pool/TaskQueue.h
@@ -0,0 +1,22 @@
+// Single-producer , single-consumer Queue
+struct SPSCQueue
+{
+    /* Define Your Data Here */
+
+} typedef SPSCQueue;
+;
+SPSCQueue *SPSCQueueInit();
+void SPSCQueuePush(SPSCQueue *pool, void *s);
+void *SPSCQueuePop(SPSCQueue *pool);
+void SPSCQueueDestory(SPSCQueue *);
+
+// Multi-producer , Multi-consumer Queue
+struct MPMCQueue
+{
+    /* Define Your Data Here */
+} typedef MPMCQueue;
+
+MPMCQueue *MPMCQueueInit(int threadNumber);
+void MPMCQueuePush(MPMCQueue *pool, void *s);
+void *MPMCQueuePop(MPMCQueue *pool);
+void MPMCQueueDestory(MPMCQueue *);
